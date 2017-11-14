03c3882f4f0c9df2c7fb0181bdaa781dc511af62
632691987 <vincentzhang@outlook.es>
2017-11-14 02:23:53 +0100

    init metrics

--- src/main/java/tina/coffee/system/config/CoffeeServerConfig.java
+++ src/main/java/tina/coffee/system/config/CoffeeServerConfig.java
@@ -1,5 +1,11 @@
 package tina.coffee.system.config;
 
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.jetty9.InstrumentedConnectionFactory;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
@@ -31,6 +37,10 @@ public class CoffeeServerConfig {
                     final QueuedThreadPool threadPool = (QueuedThreadPool)server.getThreadPool();
                     threadPool.setMaxThreads(jettyThreadMax);
                     threadPool.setMinThreads(jettyThreadMin);
+
+                    MetricRegistry metricRegistry = new MetricRegistry();
+                    final Connector connector = new ServerConnector(server, new InstrumentedConnectionFactory(new HttpConnectionFactory(), metricRegistry.timer("http.connection")));
+                    server.addConnector(connector);
                 });
         servletContainerFactory.setSessionTimeout(sessionTimeout, TimeUnit.DAYS);
         servletContainerFactory.setRegisterDefaultServlet(false);
